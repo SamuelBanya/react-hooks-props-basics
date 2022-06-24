59eb5799b663ec8f765e2fd433addf9111e4d8db
Samuel Banya <sbanya@fastmail.com>
2022-06-24 19:10:57 -0400

    commit tree 9430676db0463f3dda2b6146bd245f45d42b815d

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,32 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  console.log(props);
+
+  if (!props.isPublished) {
+    // hide unpublished content
+    // return null means "Don't display any DOM elements here"
+    return null;
+  } else {
+    // show published content
+
+    // PERSONAL REVISION TO UTILIZE IF STATEMENT FOR 'return' STATEMENT:
+    // Determine how if 'minute' word should be singular or plural
+    function determineMinuteOrMinutes(props) {
+      if (props.minutesToRead > 1) {
+        return "minutes";
+      } else {
+        return "minute";
+      }
+    }
+
+    return (
+        <div>
+        <h1>{props.articleText}</h1>
+        <p>{props.minutesToRead} {determineMinuteOrMinutes(props.minutesToRead)} to read</p>
+        </div>
+    );
+  }
 }
 
 export default BlogContent;
